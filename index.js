36f36d11b911c68c718397e1e0bf2c9e2c08e87e
Nattagorn <jay@MacBook-Air-khxng-Nattagorn.local>
2026-04-24 13:23:04 +0700

    added-updateMap

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ class Field {
     this.field = field;
     this.rows = field.length;
     this.cols = field[0].length;
-
     this.actorRow = 0;
     this.actorCol = 0;
 
@@ -31,4 +30,62 @@ class Field {
     }
     console.log("-------------------");
   }
-}
+
+  moveRight() {
+    const prevRow = this.actorRow;
+    const prevCol = this.actorCol;
+    this.actorCol += 1;
+    this.updateMap(prevRow, prevCol);
+  }
+
+  moveLeft() {
+    const prevRow = this.actorRow;
+    const prevCol = this.actorCol;
+    this.actorCol -= 1;
+    this.updateMap(prevRow, prevCol);
+  }
+
+  moveUp() {
+    const prevRow = this.actorRow;
+    const prevCol = this.actorCol;
+    this.actorRow -= 1;
+    this.updateMap(prevRow, prevCol);
+  }
+
+  moveDown() {
+    const prevRow = this.actorRow;
+    const prevCol = this.actorCol;
+    this.actorRow += 1;
+    this.updateMap(prevRow, prevCol);
+  }
+
+  updateMap(prevRow, prevCol) {
+    if (
+      this.actorRow < 0 ||
+      this.actorRow >= this.rows ||
+      this.actorCol < 0 ||
+      this.actorCol >= this.cols
+    ) {
+      console.log("🚫 You went out of bounds! Game over.");
+      process.exit();
+    }
+
+    const currentTile = this.field[this.actorRow][this.actorCol];
+
+    if (currentTile === hole) {
+      this.print();
+      console.log("💀 You fell into a hole! Game over.");
+      process.exit();
+    }
+
+    if (currentTile === hat) {
+      this.field[prevRow][prevCol] = fieldCharacter;
+      this.field[this.actorRow][this.actorCol] = pathCharacter;
+      this.print();
+      console.log("🎉 You found the hat! You win!");
+      process.exit();
+    }
+
+    this.field[prevRow][prevCol] = fieldCharacter;
+    this.field[this.actorRow][this.actorCol] = pathCharacter;
+  }}
